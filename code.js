ff28966e97200b71635753a86146b7da299dd97b
joaovitorcarreira <161030349+joaovitorcarreira@users.noreply.github.com>
2024-03-06 20:00:26 -0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,9 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
-         const texto = document.body.querySelector("#texto")
-        texto.addEventListener("click", () => {
-         if (texto.textContent==="Olá Mundo")
-        texto.textContent= "hello Word"; 
-      else
-         texto.textContent= "Olá Mundo";
-});
-}); 
+    const texto = document.body.querySelector("#texto")
+    texto.addEventListener("clik", () => {
+        if (texto.textContent==="EU AMO MINHA PRINCESA LINDA")
+        texto.textContent="EU SEMPRE VOU TE AMAR PQ VC É O AMOR DA MINHA VIDA";
+        else
+            texto.textContent= "EU AMO MINHA PRINCESA LINDA";
+  
+    });
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const texto = document.body.querySelector("#texto")
+        texto.addEventListener("clik", () => {
+            if (texto.textContent=== "A MELHOR NAMORADA DO MUNDO")
+        texto.textContent="OBRIGADO POR SEMPRE ME AJUDAR E ESTAR AO MEU LADO";
+        else   
+            texto.textContent= "A MELHOR NAMORADA DO MUNDO";
+    });
+    });
+    
+document.addEventListener("DOMContentLoaded", () => {
+        const texto = document.body.querySelector("#texto")
+        texto.addEventListener("clik", () => {
+            if (texto.textContent==="A MULHER MAIS LINDA E MAIS GOSTOSA DO MUNDO")
+            texto.textContent="A SUA BELEZA DEVERIA SER CONSIDERADA CRIME E SUA GOSTOSURA TBM";
+             else
+                texto.textContent= "A MULHER MAIS LINDA E MAIS GOSTOSA DO MUNDO";
+
+
+
+    });
+    });
+
+document.addEventListener("DOMContentLoaded", () => {
+        const texto = document.body.querySelector("#texto")
+        texto.addEventListener("clik", () => {
+            if (texto.textContent=== "MINHA PRINCESA")
+            texto.textContent="MINHA RAINHA";
+            else   
+                texto.textContent="MINHA PRINCESA";
+
+
+
+    });
+    });
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
