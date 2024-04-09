e9e67c3e26404b5ad05271596592e97ca2116a5e
JoãoCarreira <161030349+joaovitorcarreira@users.noreply.github.com>
2024-04-09 20:23:03 -0300

    code.js

--- code.js
+++ code.js
@@ -1,51 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const texto = document.body.querySelector("#texto")
     texto.addEventListener("clik", () => {
-        if (texto.textContent==="EU AMO MINHA PRINCESA LINDA")
-        texto.textContent="EU SEMPRE VOU TE AMAR PQ VC É O AMOR DA MINHA VIDA";
+        if (texto.textContent==="Ola Mundo")
+        texto.textContent="Hello Word";
         else
-            texto.textContent= "EU AMO MINHA PRINCESA LINDA";
+            texto.textContent= "Ola Mundo";
   
     });
-    });
-
-    document.addEventListener("DOMContentLoaded", () => {
-        const texto = document.body.querySelector("#texto")
-        texto.addEventListener("clik", () => {
-            if (texto.textContent=== "A MELHOR NAMORADA DO MUNDO")
-        texto.textContent="OBRIGADO POR SEMPRE ME AJUDAR E ESTAR AO MEU LADO";
-        else   
-            texto.textContent= "A MELHOR NAMORADA DO MUNDO";
-    });
-    });
-    
-document.addEventListener("DOMContentLoaded", () => {
-        const texto = document.body.querySelector("#texto")
-        texto.addEventListener("clik", () => {
-            if (texto.textContent==="A MULHER MAIS LINDA E MAIS GOSTOSA DO MUNDO")
-            texto.textContent="A SUA BELEZA DEVERIA SER CONSIDERADA CRIME E SUA GOSTOSURA TBM";
-             else
-                texto.textContent= "A MULHER MAIS LINDA E MAIS GOSTOSA DO MUNDO";
-
-
-
-    });
-    });
-
-document.addEventListener("DOMContentLoaded", () => {
-        const texto = document.body.querySelector("#texto")
-        texto.addEventListener("clik", () => {
-            if (texto.textContent=== "MINHA PRINCESA")
-            texto.textContent="MINHA RAINHA";
-            else   
-                texto.textContent="MINHA PRINCESA";
-
-
-
-    });
-    });
-
-
+ 
 
     
     
